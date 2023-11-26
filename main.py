cc97a65e82b3005d53d20fcdf59ca148c69ece13
ayoub <ayoub.ayadi.138@gmail.com>
2023-11-26 18:40:17 +0100

    Add description on the new columns

--- main.py
+++ main.py
@@ -12,6 +12,12 @@ with col2:
     content = """Hi, I am Ayoub! I am a currently an Electro- and IT Bachelor of Engineering Student in the Applied 
     Science University of Düsseldorf in Germany with a focus on IT. I have had multiple projects including Neural 
     networking for Artificial Intelligence, Web and App development, Embedded Systems Programming, Network Security 
-    and Data transmission. Below you can find some of my programming projects that i have made using Python and C#"""
+    and Data transmission."""
 
     st.info(content)
+
+content2 = """
+Below you can find some of my programming projects that i have made using Python and C#. Feel free to contact me !
+"""
+
+st.write(content2)
